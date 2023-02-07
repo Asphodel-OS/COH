55836f6cdb636ff0f3aa521a6f1d4c70b4ed828b
RedzepefendicDzejlan <dzejlan.redzepefendic@moonstruck.io>
2023-02-07 22:08:58 +0100

    feat(client): Add Phaser and network layer for multiplayer functionality. Initial Phaser game setup included.

---
+++ packages/client/src/phaser/createPhaserLayer.ts
@@ -0,0 +1,36 @@
+import { namespaceWorld } from '@latticexyz/recs';
+import CreatePhaserEngine from './engine/PhaserEngine';
+import { phaserConfig } from '../config';
+
+/**
+ * The Phaser layer is responsible for rendering game objects to the screen.
+ */
+export async function createPhaserLayer(network: any) {
+  // --- WORLD ----------------------------------------------------------------------
+  const world = namespaceWorld(network.world, 'phaser');
+
+  // --- COMPONENTS -----------------------------------------------------------------
+  const components = {};
+
+  // --- PHASER ENGINE SETUP --------------------------------------------------------
+
+  const {
+    game,
+    scenes,
+    dispose: disposePhaser,
+  } = await CreatePhaserEngine(phaserConfig as any);
+  world.registerDisposer(disposePhaser);
+
+  // --- LAYER CONTEXT --------------------------------------------------------------
+  const context = {
+    world,
+    components,
+    network,
+    game,
+    scenes
+  };
+
+  // --- SYSTEMS --------------------------------------------------------------------
+
+  return context;
+}
