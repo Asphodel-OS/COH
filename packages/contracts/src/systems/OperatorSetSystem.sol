3d158b0644a49de9128454e5a172c2faa3847fc2
ashys <40616911+chlwys@users.noreply.github.com>
2023-02-12 20:56:41 +0100

    fix: allow operator be changed

--- packages/contracts/src/systems/OperatorSetSystem.sol
+++ packages/contracts/src/systems/OperatorSetSystem.sol
@@ -20,7 +20,7 @@ contract OperatorSetSystem is System {
     uint256 ownerID = uint256(uint160(msg.sender));
     uint256 operatorID = LibOperator.getForOwner(components, ownerID);
 
-    require(operatorID == 0, "Operator: already set for owner");
+    // require(operatorID == 0, "Operator: already set for owner");
 
     operatorID = LibOperator.create(components, operator, msg.sender);
 
