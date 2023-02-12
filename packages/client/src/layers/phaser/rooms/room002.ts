30c4f1122754a8dd993d2aaa702f876a7c989250
RedzepefendicDzejlan <dzejlan.redzepefendic@moonstruck.io>
2023-02-12 21:02:28 +0100

    feat(rooms): resize map depending on resolution and added room2

--- packages/client/src/layers/phaser/rooms/room002.ts
+++ packages/client/src/layers/phaser/rooms/room002.ts
@@ -1,10 +1,17 @@
+import room002image from '../../../public/assets/room2.png';
+import { resizePicture } from '../utils/resizePicture';
+
+const scale = resizePicture();
+
 export function room002() {
   return {
+    preload: (scene: any) => {
+      scene.load.image('room002', room002image);
+    },
     create: (scene: any) => {
-      scene.add.text(50, 150, 'Room 2', {
-        fontSize: '40px',
-        color: 'white',
-      });
+      scene.add
+        .image(window.innerWidth / 2, window.innerHeight / 2, 'room002')
+        .setScale(scale * 8.3);
     },
   };
 }
