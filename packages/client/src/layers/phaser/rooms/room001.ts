ad4821770a48dd580e11f26202c5fe21b9bea58c
RedzepefendicDzejlan <dzejlan.redzepefendic@moonstruck.io>
2023-02-12 16:58:31 +0100

    feat(client): Implement state management and add object modal

--- packages/client/src/layers/phaser/rooms/room001.ts
+++ packages/client/src/layers/phaser/rooms/room001.ts
@@ -1,5 +1,6 @@
 import { PhaserScene } from '../types';
 import room001mage from '../../../public/assets/room1.png';
+import { triggerObjectModal } from '../utils/trigerObjectModal';
 
 export function room001() {
   return {
@@ -9,12 +10,12 @@ export function room001() {
     create: (scene: PhaserScene) => {
       scene.add.image(900, 500, 'room001').setScale(10);
 
-      scene.add
-        .rectangle(500, 820, 200, 180)
-        .setInteractive()
-        .on('pointerdown', () => {
-          // code for couch
-        });
+      const couch = scene.add.rectangle(500, 820, 200, 180);
+
+      triggerObjectModal(
+        couch,
+        'This couch is the perfect addition to your protected area room. With its comfortable design, this couch offers a cozy and secure place to relax.'
+      );
     },
   };
 }
