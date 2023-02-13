001df91c0e62950c480c6a0256e022c38b327e86
DzejlanRedzepefendic <dzejlanven@icloud.com>
2023-02-13 18:46:22 +0100

    fix(phaser): make object couch responsive on different devices

--- packages/client/src/layers/phaser/rooms/room001.ts
+++ packages/client/src/layers/phaser/rooms/room001.ts
@@ -2,6 +2,7 @@ import { PhaserScene } from '../types';
 import room001mage from '../../../public/assets/room1.png';
 import { triggerObjectModal } from '../utils/trigerObjectModal';
 import { resizePicture } from '../utils/resizePicture';
+import { getCouchCoordinates } from '../utils/coordinates';
 
 const scale = resizePicture();
 
@@ -11,13 +12,25 @@ export function room001() {
       scene.load.image('room001', room001mage);
     },
     create: (scene: PhaserScene) => {
-      scene.add.image(window.innerWidth / 2, window.innerHeight / 2, 'room001').setScale(scale * 8.3);
+      scene.add
+        .image(window.innerWidth / 2, window.innerHeight / 2, 'room001')
+        .setScale(scale * 8.3);
 
-      const couch = scene.add.rectangle(450, 700, 200, 180, 0xff0000);
+        console.log(scale);
+      const coordinates = getCouchCoordinates(scale);
+
+      const couch = scene.add.rectangle(
+        coordinates.x,
+        coordinates.y,
+        coordinates.width,
+        coordinates.height,
+        0xff0000
+      );
 
       triggerObjectModal(
         couch,
-        'This is a couch used for testing the item description component. ');
+        'This is a couch used for testing the item description component. '
+      );
     },
   };
 }
---
+++ packages/client/src/layers/phaser/utils/coordinates.ts
@@ -0,0 +1,18 @@
+export const getCouchCoordinates = (scale: number) => {
+  switch (scale) {
+    case 0.85:
+      return { x: 680, y: 740, width: 170, height: 170 };
+    case 0.82:
+      return { x: 490, y: 640, width: 150, height: 150 };
+    case 0.75:
+      return { x: 420, y: 610, width: 150, height: 150 };
+    case 0.5:
+      return { x: 170, y: 900, width: 170, height: 170 };
+    case 0.33:
+      return { x: 170, y: 1250, width: 170, height: 170 };
+    case 0.15:
+      return { x: 420, y: 1150, width: 150, height: 150 };
+    default:
+      return { x: 170, y: 900, width: 170, height: 170 };
+  }
+};
