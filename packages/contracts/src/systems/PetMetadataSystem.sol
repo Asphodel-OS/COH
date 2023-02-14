36ba215013d660633ee6892808008fb73d0d5dd4
ashys <40616911+chlwys@users.noreply.github.com>
2023-02-14 11:21:25 +0100

    start petmetadatasystem

---
+++ packages/contracts/src/systems/PetMetadataSystem.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import { IWorld } from "solecs/interfaces/IWorld.sol";
+import { System } from "solecs/System.sol";
+import { getAddressById } from "solecs/utils.sol";
+
+import { LibPet } from "libraries/LibPet.sol";
+import { LibPetTraits } from "libraries/LibPetTraits.sol";
+
+import { ID as PetSystemID } from "systems/ERC721PetSystem.sol";
+
+uint256 constant ID = uint256(keccak256("system.ERC721.metadata"));
+
+contract PetMetadataSystem is System {
+  constructor(IWorld _world, address _components)
+    System(_world, _components)
+  {}
+
+  /*********************
+   *  PET SET PIC
+  **********************/
+
+  modifier onlyPetSystem() {
+    require(
+      msg.sender == getAddressById(components, PetSystemID),
+      "not Pet System"
+    );
+    _;
+  }
+
+  function execute(bytes memory arguments) public onlyPetSystem returns (bytes memory) {
+    require(false, "execute not available");
+    arguments = "";
+    return "";
+  }
+
+  function executeTyped(address ownerAddy) public onlyPetSystem returns (bytes memory) {
+    return execute(abi.encode(ownerAddy));
+  }
+
+  /*********************
+   *  METADATA ASSEMBLER
+  **********************/
+
+  function tokenURI(uint256 tokenID) public view returns (string memory) {
+    uint256 petID = LibPet.indexToID(components, tokenID);
+    return LibPet.getMediaURI(components, petID);
+  }
+}
