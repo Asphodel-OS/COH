63f1467d569398d9b42b221181df6327de9d60dd
Lethe <102979407+asphodellethe@users.noreply.github.com>
2023-02-14 18:05:34 +0000

    fix: inventory button

--- packages/client/src/layers/react/components/index.ts
+++ packages/client/src/layers/react/components/index.ts
@@ -4,6 +4,7 @@ import { registerMerchantWindow } from './MerchantWindow';
 import { registerMiningModal } from './MiningModal';
 import { registerObjectModal } from './ObjectModal';
 import { registerInventory } from './Inventory';
+import { registerInventoryButton } from './InventoryButton';
 import { registerPetList } from './PetList';
 import { registerRequestQueue } from './RequestQueue';
 import { registerTradeWindow } from './TradeWindow';
@@ -18,4 +19,5 @@ export function registerUIComponents() {
   registerMiningModal();
   registerRequestQueue();
   registerTradeWindow();
+  registerInventoryButton();
 }
