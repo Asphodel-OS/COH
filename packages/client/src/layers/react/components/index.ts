1fa5679558de518da8e576181e22bea5305e4075
RedzepefendicDzejlan <dzejlan.redzepefendic@moonstruck.io>
2023-02-14 19:58:49 +0100

    fix(inventory): now it pops modal onClick

--- packages/client/src/layers/react/components/index.ts
+++ packages/client/src/layers/react/components/index.ts
@@ -4,7 +4,7 @@ import { registerMerchantWindow } from './MerchantWindow';
 import { registerMiningModal } from './MiningModal';
 import { registerObjectModal } from './ObjectModal';
 import { registerInventory } from './Inventory';
-import { registerInventoryButton } from './InventoryButton';
+import { registerInventoryButton } from './inventoryButton';
 import { registerPetList } from './PetList';
 import { registerRequestQueue } from './RequestQueue';
 import { registerTradeWindow } from './TradeWindow';
