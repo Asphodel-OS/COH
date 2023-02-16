0667153b3b5b2f8a238554d37d9a0bdb2254c45b
Lethe <102979407+asphodellethe@users.noreply.github.com>
2023-02-16 19:38:20 +0000

    fix: dialogue sound

--- packages/client/src/layers/phaser/utils/triggerObjectModal.ts
+++ packages/client/src/layers/phaser/utils/triggerObjectModal.ts
@@ -1,10 +1,13 @@
 import { dataStore } from '../../react/store/createStore';
+import dialogueSound from '../../../public/sound/sound_effects/speech1.wav'
 
 export const triggerObjectModal = (
   object: Phaser.GameObjects.GameObject,
   description: string
 ) => {
   return object.setInteractive().on('pointerdown', () => {
+    const clickFX = new Audio(dialogueSound)
+    clickFX.play()
     const objectId = document.getElementById('object_modal');
     if (objectId) {
       objectId.style.display = 'block';
