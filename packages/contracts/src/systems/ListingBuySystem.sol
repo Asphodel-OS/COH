3ef10c1eef7fbf543c84b230e8ea29e4ec8ee8a0
jirach <acheron@asphodel.io>
2023-02-17 11:40:25 -0700

    fix(listings): resolve bugs/failures

--- packages/contracts/src/systems/ListingBuySystem.sol
+++ packages/contracts/src/systems/ListingBuySystem.sol
@@ -18,8 +18,9 @@ contract ListingBuySystem is System {
   function execute(bytes memory arguments) public returns (bytes memory) {
     (uint256 listingID, uint256 amt) = abi.decode(arguments, (uint256, uint256));
     uint256 operatorID = LibOperator.getByAddress(components, msg.sender);
+    uint256 merchantID = LibListing.getMerchant(components, listingID);
 
-    require(Utils.sameRoom(components, listingID, operatorID), "Merchant: must be in room");
+    require(Utils.sameRoom(components, merchantID, operatorID), "Merchant: must be in room");
 
     // create an inventory for the operator first if one doesn't exist
     uint256 itemIndex = LibListing.getItemIndex(components, listingID);
