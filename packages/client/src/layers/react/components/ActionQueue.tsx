76528137fb36005f13b80e7868d572ed1ae42610
Lethe <102979407+asphodellethe@users.noreply.github.com>
2023-02-17 13:26:09 +0000

    fix: better actionqueue design, r1 music

--- packages/client/src/layers/react/components/ActionQueue.tsx
+++ packages/client/src/layers/react/components/ActionQueue.tsx
@@ -6,15 +6,16 @@ import {
 import { map } from 'rxjs';
 import { ActionStateString, ActionState } from '@latticexyz/std-client';
 import { registerUIComponent } from '../engine/store';
+import styled from 'styled-components';
 
 export function registerActionQueue() {
   registerUIComponent(
     'ActionQueue',
     {
-      rowStart: 3,
-      rowEnd: 30,
-      colStart: 34,
-      colEnd: 62,
+      rowStart: 40,
+      rowEnd: 70,
+      colStart: 87,
+      colEnd: 96,
     },
     (layers) => {
       const {
@@ -31,19 +32,50 @@ export function registerActionQueue() {
     },
     ({ Action }) => {
       return (
-        <div>
-          <p>Actions:</p>
+        <ModalWrapper>
+          <ModalContent>
+          <Description>Actions:</Description>
           {[...getComponentEntities(Action)].map((e) => {
             const actionData = getComponentValueStrict(Action, e);
             const state = ActionStateString[actionData.state as ActionState];
             return (
-              <p key={`action${e}`}>
+              <Description key={`action${e}`}>
                 {Action.world.entities[e]}: {state}
-              </p>
+              </Description>
             );
           })}
-        </div>
+          </ModalContent>
+        </ModalWrapper>
       );
     }
   );
 }
+
+const ModalWrapper = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  padding: 20px;
+  width: 99%;
+  border-style: solid;
+  border-width: 2px;
+  border-color: black;
+`;
+
+const Description = styled.p`
+  font-size: 14px;
+  color: #333;
+  text-align: left;
+  padding: 2px;
+  font-family: Pixel;
+`;
